e0ea5042560fd4ed4e79fec4d8ce5cfecb656edd
n.alipkin_pinkman <n.alipkin@pinkman.ru>
2021-02-09 16:39:05 +0300

    50mb limit body parser

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const WSServer = require('express-ws')(app);
 const aWss = WSServer.getWss();
@@ -9,8 +10,8 @@ const path = require('path');
 
 app.use(cors())
 app.use(express.json())
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb'}));
+app.use(bodyParser.json({limit: '10mb', extended: true}))
+app.use(bodyParser.urlencoded({limit: '10mb', extended: true}))
 
 app.ws('/', (ws) => {
   ws.on('message', data => {

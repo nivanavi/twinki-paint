01e1a2db259efa854f12f5fd966d92034d3eaa9e
n.alipkin_pinkman <n.alipkin@pinkman.ru>
2021-02-09 16:33:12 +0300

    50mb limit

--- server/index.ts
+++ server/index.ts
@@ -9,6 +9,8 @@ const path = require('path');
 
 app.use(cors())
 app.use(express.json())
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 
 app.ws('/', (ws) => {
   ws.on('message', data => {
